9b13540e43168b72850b5007666bf643c2ef0041
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-11-03 18:42:34 -0300

    pagina de Sign Up criada, ainda sem funcionalidades

--- front-end/src/Routes/Routes.js
+++ front-end/src/Routes/Routes.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Login, Tasks } from '../Pages/Index';
+import { Login, Tasks, SignUp } from '../Pages/Index';
 
 function Routes() {
   return (
     <Switch>
       <Route exact path='/' component={ Login } />
+      <Route exact path='/signup' component={ SignUp } />
       <Route exact path='/tasks' component={ Tasks } />
     </Switch>
   )
