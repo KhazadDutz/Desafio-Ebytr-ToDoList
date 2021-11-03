0ebac40bb20f8a276642b925cd4a55c39f7054fe
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-11-03 18:09:44 -0300

    Provider configurado, context criado

--- front-end/src/Providers/appProvider.js
+++ front-end/src/Providers/appProvider.js
@@ -1,1 +1,16 @@
+import appContext from '../Context/appContext';
 import React, { useState } from 'react';
+
+function appProvider({ children }) {
+
+  const data = {};
+
+  return (
+    <appContext.Provider value={ data }>
+      { children }
+    </appContext.Provider>
+  )
+}
+
+
+export default appProvider
