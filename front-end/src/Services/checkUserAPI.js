bfd5f8983d8a7f9e5f25edb3c12ce3da55ff362c
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-11-04 17:37:50 -0300

    tentativa de configuar e utilizar o axios

--- front-end/src/Services/checkUserAPI.js
+++ front-end/src/Services/checkUserAPI.js
@@ -1,18 +1,26 @@
+const axios = require('axios');
+
 async function checkUserAPI(URL, email, password) {
-  const options = {
-    method: 'POST',
-    body: JSON.stringify({
-      email,
-      password,
-    })
-  }
-  try {
-    const response = await fetch(`http://localhost:3001${URL}`, options)
-    const data = await response.json();
-    return response.ok ? Promise.resolve(data) : Promise.reject(data);
-  } catch (error) {
-    return { message: error };
-  }
+  // const options = {
+  //   method: 'POST',
+  //   body: JSON.stringify({
+  //     email,
+  //     password,
+  //   })
+  // }
+  // try {
+    // const response = await fetch(`http://localhost:3001${URL}`, options)
+    // const data = await response.json();
+    // return response.ok ? Promise.resolve(data) : Promise.reject(data);
+    // } catch (error) {
+      //   return { message: error };
+      // }
+  axios.post(`http://localhost:3001${URL}`, {
+    email,
+    password,
+  })
+  .then((data) => data ? Promise.resolve(data) : Promise.reject(data))
+  .catch((error) =>  console.log(error));
 }
 
 export default checkUserAPI;
